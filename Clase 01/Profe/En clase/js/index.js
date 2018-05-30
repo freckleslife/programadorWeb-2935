cb230455e57989d4d0e0913e3701d86161e43f63
Mar <MLC@Marcelas-Macintosh.local>
2018-05-29 21:22:08 -0300

    Pepe

--- Clase 01/Profe/En clase/js/index.js
+++ Clase 01/Profe/En clase/js/index.js
@@ -1,13 +1,61 @@
 // Primera clase
 
+
 // 5. Variables
+
+// var fullName
+
+// console.log('hoLA 0 ' + fullName)
+
+// fullName = 'Adriaán Ferré'
+
+// console.log('Hola 1 ' + fullName)
+
+// fullName = 'Marcela Correa'
+
+// console.log('Hola ' + fullName)
+
+
+
 
 // 6. Tipos de valores
 // Boolean
+var value1 = false
+
+var value2 = true
+
 // Number
+
+var number1 = 2
+
+var number = -5
+
+var pi = 3.1416
+
 // String
+
+var fullName = 'Marcela Correa'
+
+var text = 'lalalala lalalala lalalala'
+
+var message = 'Hola Pedro'
+
+var message2 = "Hola 'juan'"
+
 // null / undefined
+
+var undefinedValue = undefined
+var nullValue = null
+
 // Array
+
+var examsResults = [10, 9, 8, 5]
+
+examsResults[3] = 8
+
+console.log(examsResults[3])
+
+var arrayAnyValue = [2, 'Ad', false, [1, 2, 3]]
 // Object
 
 // 7. Operadores aritméticos
